a740c9822e82e524b16568c5c846d0473395f50e
Chien Nguyen <36409877+fankao@users.noreply.github.com>
2023-08-12 14:01:42 +0700

    MAKING A DOMAIN CLASS PERSISTENT

--- src/main/java/com/polarbookshop/catalogservice/domain/Book.java
+++ src/main/java/com/polarbookshop/catalogservice/domain/Book.java
@@ -1,12 +1,16 @@
 package com.polarbookshop.catalogservice.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 
 public record Book (
-
+    @Id
+    Long id, //  Identifies the field as the primary key for the entity
     @NotBlank(message = "The book ISBN must be defined.")
     @Pattern(regexp = "^([0-9]{10}|[0-9]{13})$", message = "The ISBN format must be valid.")
     String isbn,
@@ -19,6 +23,15 @@ public record Book (
 
     @NotNull(message = "The book price must be defined.")
     @Positive(message = "The book price must be greater than zero.")
-    Double price
+    Double price,
+
+    @Version
+    int version //  The entity version number, which is used for optimistic locking
     
-){}
+){
+
+        public static Book of(String isbn, String title, String author, Double price) {
+            // An entity is considered new when the ID is null and the version is 0.
+            return new Book(null,isbn, title, author, price,0);
+        }
+}
